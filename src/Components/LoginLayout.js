d68b106a676bfe908ddfbdb77d01fbb6bad94943
nireeksha.jain <ss.jain@accolitedigital.com>
2023-06-01 20:43:52 +0530

    shop

---
+++ src/Components/LoginLayout.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import Login from './Login';
+// import RegisterLayout from './RegisterLayout';
+
+// const LoginLayout = () => {
+//   return (
+//     <div>
+//       <Login />
+//       <p>New user? <Link to="/register">Register here</Link></p>
+//     </div>
+//   );
+// };
+
+// export default LoginLayout;
